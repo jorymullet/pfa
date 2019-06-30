b61a86b497c63675dda9c85c4307da0ce85c3af4
Joshua Mullet <jorymullet@gmail.com>
2019-06-30 16:26:31 -0600

    started on footer

--- src/components/routed/Campaign/Footer.vue
+++ src/components/routed/Campaign/Footer.vue
@@ -1,6 +1,36 @@
 <script>
 export default {
   name: 'CampaignFooter',
+  data () {
+    return {
+      footerLinks: [
+        {
+          name: 'HOME',
+          to: 'CampaignHome',
+        },
+        {
+          name: 'ISSUES',
+          to: 'CampaignHome',
+        },
+        {
+          name: 'MEET PETE',
+          to: 'CampaignHome',
+        },
+        {
+          name: 'EVENTS',
+          to: 'CampaignHome',
+        },
+        {
+          name: 'STORE',
+          to: 'CampaignHome',
+        },
+        {
+          name: 'DONATE',
+          to: 'CampaignHome',
+        },
+      ],
+    }
+  },
 }
 </script>
 
@@ -8,6 +38,9 @@ export default {
   .campaign-footer-main
     .campaign-footer-container
       .content
+        .footer-nav
+        .misc
+        .emblem
 </template>
 
 <style lang="sass" scoped>
@@ -17,4 +50,6 @@ export default {
       .content
         padding: 0 15px
         margin: 0 auto
+        display: grid
+        grid-template-columns: 33% 33% 33%
 </style>
